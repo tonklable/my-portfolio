a30c6c587309638be3025533a5e5b3c166ea4b6a
tonklable <tonklalor2544@gmail.com>
2023-04-24 12:04:59 +0900

    resume edit

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -5,24 +5,28 @@ import { motion, AnimatePresence } from "framer-motion";
 export default function Resume() {
     return (
 
-        <motion.div layout
+        <motion.div className='items-center justify-center' layout
             transition={{ type: "spring" }}>
             <Head>
                 <title>Resume</title>
             </Head>
-            <div className="px-8 flex justify-center items-center h-screen">
-                <div className="flex items-center justify-center w-5/6 h-5/6 bg-white shadow-lg">
-                    <div className="relative w-full h-full p-4">
-                        <Image src={'/img/Resume_Mercari_SE.png'} layout="fill" objectFit="contain" alt="Resume" />
+            <div className='relative w-screen h-auto mx-auto'>
+                <h1 className="text-xl lg:text-3xl text-left md:my-8 font-bold text-black font-sans leading-tight px-8 md:px-16 mt-8">
+                    Resume 🗎
+                </h1>
+                <div className="grid grid-cols-1 md:grid-cols-3">
+                    <div className="flex order-2 md:order-1 col-span-2 m-auto bg-white shadow-lg">
+                        <Image src={'/img/Resume_Mercari_SE.png'} className='max-w-xs md:max-w-md lg:max-w-2xl' layout="responsive" width="90" height="160" alt="Resume" />
                         <div className="absolute inset-0 bg-black opacity-25"></div>
                     </div>
-                </div>
-                <div className="flex flex-col items-center justify-center w-1/2 h-full bg-gray-100">
-                    <a href="/Resume_Mercari_SE.pdf" download className="mt-4 py-2 px-4 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
-                        Download
-                    </a>
+                    <div className="col-span-1 order-1 flex flex-col mx-auto md:ml-0 h-full bg-gray-100">
+                        <a href="/Resume_Mercari_SE.pdf" download className="my-4 md:my-0 py-2 px-4 bg-main text-white md:font-semibold rounded-lg shadow-md ">
+                            Download (.pdf)
+                        </a>
+                    </div>
                 </div>
             </div>
+
         </motion.div >
     )
 }
